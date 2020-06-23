de26c7ad970b7f065a356118ed97fa0c64ac4c35
FlyerB <zarandazchi@gmail.com>
2020-06-23 09:38:44 -0500

    added asp- routing to Admin Index and deleted test surveys

--- Survey_Project/Controllers/SurveyController.cs
+++ Survey_Project/Controllers/SurveyController.cs
@@ -165,8 +165,9 @@ namespace Survey_Project.Controllers
             var survey = _context.Surveys.Where(s => s.SurveyId == id).FirstOrDefault();
             _context.Surveys.Remove(survey);
             _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
-           
+            return RedirectToAction("SurveyQuestions");
+
+
         }
     }
 }
