9cf1d15f1e0c9287c501bb770b9065266fc88fb0
FlyerB <zarandazchi@gmail.com>
2020-06-25 21:44:15 -0500

    changed Admin view to @survey

---
+++ Survey_Project/Controllers/ChartsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Survey_Project.Controllers
+{
+    public class ChartsController : Controller
+    {
+        [Route("product")]
+        public class ProductController : Controller
+        {
+            [Route("")]
+            [Route("index")]
+            [Route("~/")]
+            public IActionResult Index()
+            {
+                return View();
+            }
+        }
+    }
+}
